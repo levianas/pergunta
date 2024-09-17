b44db50c3f03dbffeac1b7f8e58a25c8fe2a18d3
levianas <160236036+levianas@users.noreply.github.com>
2024-09-17 10:59:38 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,27 +1,27 @@
 criaCartao(
     'física',
     'como se calcula a força?',
-    F=M*A'),
+    'F=M*A')
     
     criaCartão(
     'física',
     'como se calcula a pressão?',
-    P=F/A'),
+    'P=F/A')
     
     criaCartao(
     'história',
     'em que ano começou a primeira guerra mundial?'
-    em 1945')
+    'em 1945')
     
     criaCartao(
     'história',
     'quando ocorreu o golpe do estado novo?',
-    em 1937')
+    'em 1937')
     
     criaCartao(
     'biologia',
     'quantos reinos existem na biologia?',
-    5')
+    '5')
     
     criaCartao(
     'biologia',
@@ -31,57 +31,57 @@ criaCartao(
     criaCartao(
     'quimica',
     'qual a formula quimica do ácido sulfúrico?',
-    H2SO4')
+    'H2SO4')
     
     criaCartao(
     'química',
     'qual o pH da soda cáustica?',
-    14')
+    '14')
     
     criaCartao(
     'geografia',
     'em que continente fica Brunei?',
-    Ásia')
+    'Ásia')
     
     criaCartao(
     'geografia',
     'qual a capital do México?',
-    Cidade do México')
+    'Cidade do México')
     
     criaCartao(
     'sociologia',
     'qual a principal teoria de Durkheim?',
-    Fato social')
+    'Fato social')
     
     criaCartao(
     'sociologia',
     'quem foi o pai do comunismo?',
-    Karl Marx')
+   'Karl Marx')
     
     criaCartao(
     'matemática',
     'como se calcula a hipotenusa?',
-    A²+B²=C²')
+    'A²+B²=C²')
     
     criaCartao(
     'matematica',
     'quanto é 25% de 10000?',
-    2500'),
+    '2500'),
     
     criaCartao(
     'historia',
     'quando começou a primeira guerra mundial?',
-    1914'),
+    '1914'),
     
     criaCartao(
     'historia',
     'qual a data da independência do Brasil?',
-    7 de setembro de 1822')
+    '7 de setembro de 1822')
     
     criaCartao(
     'biologia',
     'qual é o sistema em que o coração está?',
-    sistema cardiovascular')
+    'sistema cardiovascular')
     
     criaCartao(
     'biologia',
@@ -91,9 +91,9 @@ criaCartao(
     criaCartao(
     'física',
     'qual fisico que escreveu sobre a teoria da gravitação universal?',
-    Isaac Newton')
+    'Isaac Newton')
     
     criaCartao(
     'quimica',
     'quantas ligações o carbono faz?',
-    4 ligações')
+    '4 ligações')
