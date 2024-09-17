9e87325927cea6975d742d7f74080d42826963d8
levianas <160236036+levianas@users.noreply.github.com>
2024-09-17 10:57:20 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,119 +1,99 @@
 criaCartao(
     'física',
-    'como se calcula a força?'
-    'Utiliza a formula:
-    F=M*A').
+    'como se calcula a força?',
+    F=M*A'),
     
     criaCartão(
     'física',
     'como se calcula a pressão?',
-    'utiliza a formula:
-    P=F/A').
+    P=F/A'),
     
     criaCartao(
-    'história'
+    'história',
     'em que ano começou a primeira guerra mundial?'
-    'resposta:
     em 1945')
     
     criaCartao(
-    'história'
+    'história',
     'quando ocorreu o golpe do estado novo?',
-    'resposta:
     em 1937')
     
     criaCartao(
-    'biologia'
+    'biologia',
     'quantos reinos existem na biologia?',
-    'resposta:
     5')
     
     criaCartao(
-    'biologia'
+    'biologia',
     'quais são os macronutrientes mais importantes?',
-    'resposta:
     proteina, carboidratos e lipídios')
     
     criaCartao(
-    'quimica'
+    'quimica',
     'qual a formula quimica do ácido sulfúrico?',
-    'resposta:
     H2SO4')
     
     criaCartao(
-    'química'
+    'química',
     'qual o pH da soda cáustica?',
-    'resposta:
     14')
     
     criaCartao(
-    'geografia'
-    'em que continente fica Brunei?'
-    'resposta:
+    'geografia',
+    'em que continente fica Brunei?',
     Ásia')
     
     criaCartao(
-    'geografia'
-    'qual a capital do México?'
-    'resposta:
+    'geografia',
+    'qual a capital do México?',
     Cidade do México')
     
     criaCartao(
-    'sociologia'
-    'qual a principal teoria de Durkheim?'
-    'resposta:
+    'sociologia',
+    'qual a principal teoria de Durkheim?',
     Fato social')
     
     criaCartao(
-    'sociologia'
-    'quem foi o pai do comunismo?'
-    'resposta:
-    Karl Marx’)
+    'sociologia',
+    'quem foi o pai do comunismo?',
+    Karl Marx')
     
     criaCartao(
-    'matemática'
-    'como se calcula a hipotenusa?'
-    'resposta:
-    A²+B²=C²’)
+    'matemática',
+    'como se calcula a hipotenusa?',
+    A²+B²=C²')
     
     criaCartao(
-    'matematica'
-    'quanto é 25% de 10000?'
-    'resposta:
-    2500’)
+    'matematica',
+    'quanto é 25% de 10000?',
+    2500'),
     
     criaCartao(
-    'historia'
-    'quando começou a primeira guerra mundial?'
-    'resposta:
-    1914')
+    'historia',
+    'quando começou a primeira guerra mundial?',
+    1914'),
     
     criaCartao(
-    'historia'
-    'qual a data da independência do Brasil?'
-    'resposta:
+    'historia',
+    'qual a data da independência do Brasil?',
     7 de setembro de 1822')
     
     criaCartao(
-    'biologia'
-    'qual é o sistema em que o coração está?'
-    'resposta:
+    'biologia',
+    'qual é o sistema em que o coração está?',
     sistema cardiovascular')
     
     criaCartao(
-    'biologia'
-    'quais são os tipos de celulas q existem?'
-    'resposta:
+    'biologia',
+    'quais são os tipos de celulas q existem?',
     'eucarionte e procarionte')
     
     criaCartao(
-    'física'
-    'qual fisico que escreveu sobre a teoria da gravitação universal?'
-    'resposta:
+    'física',
+    'qual fisico que escreveu sobre a teoria da gravitação universal?',
     Isaac Newton')
     
     criaCartao(
-    'quimica'
-    'quantas ligações o carbono faz?'
-    'resposta:
+    'quimica',
+    'quantas ligações o carbono faz?',
     4 ligações')
