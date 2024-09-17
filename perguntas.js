448d3ce2f5f9ab39f1b19c76732e8297cfe7a851
levianas <160236036+levianas@users.noreply.github.com>
2024-09-17 10:48:48 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'física',
+    'como se calcula a força?'
+    'Utiliza a formula:
+    F=M*A').
+    
+    criaCartão(
+    'física’,
+    'como se calcula a pressão?',
+    'utiliza a formula:
+    P=F/A').
+    
+    criaCartao(
+    'história'
+    'em que ano começou a primeira guerra mundial?'
+    'resposta:
+    em 1945')
+    
+    criaCartao(
+    'história'
+    'quando ocorreu o golpe do estado novo?',
+    'resposta:
+    em 1937')
+    
+    criaCartao(
+    'biologia'
+    'quantos reinos existem na biologia?',
+    'resposta:
+    5')
+    
+    criaCartao(
+    'biologia'
+    'quais são os macronutrientes mais importantes?',
+    'resposta:
+    proteina, carboidratos e lipídios')
+    
+    criaCartao(
+    'quimica'
+    'qual a formula quimica do ácido sulfúrico?',
+    'resposta:
+    H2SO4')
+    
+    criaCartao(
+    'química'
+    'qual o pH da soda cáustica?',
+    'resposta:
+    14')
+    
+    criaCartao(
+    'geografia'
+    'em que continente fica Brunei?'
+    'resposta:
+    Ásia')
+    
+    criaCartao(
+    'geografia'
+    'qual a capital do México?'
+    'resposta:
+    Cidade do México')
+    
+    criaCartao(
+    'sociologia'
+    'qual a principal teoria de Durkheim?'
+    'resposta:
+    Fato social')
+    
+    criaCartao(
+    'sociologia'
+    'quem foi o pai do comunismo?'
+    'resposta:
+    Karl Marx’)
+    
+    criaCartao(
+    'matemática'
+    'como se calcula a hipotenusa?'
+    'resposta:
+    A²+B²=C²’)
+    
+    criaCartao(
+    'matematica'
+    'quanto é 25% de 10000?'
+    'resposta:
+    2500’)
+    
+    criaCartao(
+    'historia'
+    'quando começou a primeira guerra mundial?'
+    'resposta:
+    1914')
+    
+    criaCartao(
+    'historia'
+    'qual a data da independência do Brasil?'
+    'resposta:
+    7 de setembro de 1822')
+    
+    criaCartao(
+    'biologia'
+    'qual é o sistema em que o coração está?'
+    'resposta:
+    sistema cardiovascular')
+    
+    criaCartao(
+    'biologia'
+    'quais são os tipos de celulas q existem?'
+    'resposta:
+    'eucarionte e procarionte')
+    
+    criaCartao(
+    'física'
+    'qual fisico que escreveu sobre a teoria da gravitação universal?'
+    'resposta:
+    Isaac Newton')
+    
+    criaCartao(
+    'quimica'
+    'quantas ligações o carbono faz?'
+    'resposta:
+    4 ligações')
