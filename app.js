c679382715b53f90c834684bf6b35fe18daf36f1
levianas <160236036+levianas@users.noreply.github.com>
2024-09-24 09:09:56 -0300

    sd

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
-    let cartao = document.createElement('article')
+    let cartao = document.createElement('article');
     cartao.className = 'cartao';
 
     cartao.innerHTML = `
@@ -14,5 +14,5 @@ function criaCartao(categoria, pergunta, resposta){
     </div>
     </div>
     `
-    container.appendChild(cartao)
+    container.appendChild(cartao);
 }
